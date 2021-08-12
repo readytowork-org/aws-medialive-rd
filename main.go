2cb12cbd4351c596b669e9a059d9aa0281ea8556
esprajwol <esprajwol@gmail.com>
2021-08-12 22:36:00 +0545

    chore: set env

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -12,18 +13,29 @@ import (
 )
 
 func main() {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"), config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider("", "", "")))
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("ap-northeast-1"), config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(os.Getenv("AWS_KEY"), os.Getenv("AWS_SECRET"), "")))
 	if err != nil {
 		log.Fatalf("unable to load SDK config, %v", err)
 	}
 
 	// Using the Config value, create the DynamoDB client
 	svc := medialive.NewFromConfig(cfg)
-	out, err := svc.StartChannel(context.TODO(), &medialive.StartChannelInput{
+
+	// start channel
+	/*out, err := svc.StartChannel(context.TODO(), &medialive.StartChannelInput{
 		ChannelId: aws.String("5867858"),
 	})
 	if err != nil {
-		log.Fatalf("unable to load channel start", err)
+		log.Fatalf("unable to load channel start: ", err)
+	}
+	*/
+
+	// stop channel
+	out, err := svc.StopChannel(context.TODO(), &medialive.StopChannelInput{
+		ChannelId: aws.String("5867858"),
+	})
+	if err != nil {
+		log.Fatalln("unable to load channel stop: ", err)
 	}
 
 	fmt.Println(out)
